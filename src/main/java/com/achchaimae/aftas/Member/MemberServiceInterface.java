ea538d355c4c53c07c7f5a0cb7e943ce2d45b6f3
achchaimae <achchaimae1@gmail.com>
2023-12-08 15:29:47 +0100

    AF-7 AF-8 AF-9 AF-10 AF-11 crud of member

--- src/main/java/com/achchaimae/aftas/Member/MemberServiceInterface.java
+++ src/main/java/com/achchaimae/aftas/Member/MemberServiceInterface.java
@@ -1,4 +1,16 @@
 package com.achchaimae.aftas.Member;
 
+import com.achchaimae.aftas.Member.DTO.MemberReqDTO;
+import com.achchaimae.aftas.Member.DTO.MemberRespDTO;
+
+import java.util.List;
+
 public interface MemberServiceInterface {
+     List<MemberRespDTO> getMembers();
+    MemberRespDTO saveMember(MemberReqDTO member);
+    MemberRespDTO updateMember(MemberReqDTO member, Integer id);
+    Integer DeleteMember(Integer memberId);
+    MemberRespDTO findMember(Integer mediaId);
+
+
 }
