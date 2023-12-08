f3e05f444a3d2bdfd3f508cb975e0a5509363fb6
achchaimae <achchaimae1@gmail.com>
2023-12-08 15:19:33 +0100

    AF-7 AF-8 AF-9 AF-10 AF-11 crud of member

--- src/main/java/com/achchaimae/aftas/Member/DTO/MemberRespDTO.java
+++ src/main/java/com/achchaimae/aftas/Member/DTO/MemberRespDTO.java
@@ -3,13 +3,12 @@ package com.achchaimae.aftas.Member.DTO;
 import com.achchaimae.aftas.Hunting.DTO.HuntingReqDTO;
 import com.achchaimae.aftas.Member.Enum.IdentityDocumentType;
 import com.achchaimae.aftas.Ranking.DTO.RankingReqDTO;
-import com.achchaimae.aftas.Ranking.Ranking;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
-public class MemeberRespDTO {
+public class MemberRespDTO {
     private int num;
     private String name ;
     private String familtyName ;
---
+++ src/main/java/com/achchaimae/aftas/Member/MemberController.java
@@ -0,0 +1,4 @@
+package com.achchaimae.aftas.Member;
+
+public class MemberController {
+}
